ad2dd241797ec7e5ff785fa493e8a8a91cf01674
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-02 08:19:44 +0530

    initial update

---
+++ src/org/santhoshkumar/BinarySearchTree.java
@@ -0,0 +1,11 @@
+package org.santhoshkumar;
+
+import java.io.*;
+import java.util.*;
+
+public class BinarySearchTree {
+
+    public static void main(String[] args) {
+        System.out.println("BST");
+    }
+}
