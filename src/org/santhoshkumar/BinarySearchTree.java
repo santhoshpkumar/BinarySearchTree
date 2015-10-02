628fbb5f3f7b24a8e3b3b3c895e23df40db5bf6a
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-10-02 09:34:30 +0530

    vertical order implementation

--- src/org/santhoshkumar/BinarySearchTree.java
+++ src/org/santhoshkumar/BinarySearchTree.java
@@ -6,6 +6,8 @@ import java.util.*;
 public class BinarySearchTree {
 
     Node root;
+    TreeMap<Integer,ArrayList> vertical;
+    Queue<Node> queue;
 
     public static void main(String[] args) {
         System.out.println("Binary Search Tree");
@@ -20,7 +22,12 @@ public class BinarySearchTree {
         bst.insert(90);
         bst.insert(95);
 
+        bst.insert(91);
+        bst.insert(21);
+        bst.insert(31);
+
         bst.inOrder(bst.root);
+        bst.verticalOrder();
     }
 
     public void insert(int data ){
@@ -74,6 +81,31 @@ public class BinarySearchTree {
         postOrder(node.left);
         postOrder(node.right);
         System.out.println(node.data);
+    }
+
+    public void verticalOrder(){
+        vertical = null;
+        vertical = new TreeMap<Integer,ArrayList>();
+        buildVerticalOrder(root, 0);
+
+        Set<Integer> keys = vertical.keySet();
+        for(int key:keys){
+            System.out.println(vertical.get(key));
+        }
+    }
+
+    public void buildVerticalOrder(Node node, int level){
+        if (node == null){
+            return;
+        }
+        ArrayList values = vertical.get(level);
+        if (values == null){
+            values= new ArrayList();
+        }
+        values.add(node.data);
+        vertical.put(level,values);
+        buildVerticalOrder(node.left,level-1);
+        buildVerticalOrder(node.right,level+1);
     }
 }
 
